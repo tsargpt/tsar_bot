6e8d48ed740e8c88b6ff9309ed51efd6ec8614b4
tsargpt <tsar_bot@outlook.com>
2025-03-05 20:42:41 +0300

    fix regular expressions fix answer +religion handler

--- app/group/handlers.py
+++ app/group/handlers.py
@@ -9,17 +9,22 @@ router = Router()
 router.message.filter(F.chat.type.in_({"group", "supergroup"}))
 
 
-@router.message(MaskedTextFilter(r"\b(ч+у+р+к+а+)\b"))
+@router.message(MaskedTextFilter(r"\b(ч+у+р+к+а*(?:а|и|у|е|ой|ами|ах)?)\b"))
 async def handle_masked_text(message: Message, word: str):
     await message.answer(LEXICON_RU["чурка"].format(word=word))
 
 
 @router.message(MaskedTextFilter(r"\b(и+с+л+а+м+)\b"))
-async def handle_masked_text(message: Message, word: str):
-    await message.answer(LEXICON_RU["ислам"].format(word=word))
+async def handle_masked_text(message: Message):
+    await message.answer(LEXICON_RU["ислам"])
 
 
 @router.message(MaskedTextFilter(r"\b(м+и+т+)\b"))
-async def handle_masked_text(message: Message, word: str):
+async def handle_masked_text(message: Message):
     random_num = random.randint(1, 4)
-    await message.answer(LEXICON_RU[f"мит{random_num}"].format(word=word))
+    await message.answer(LEXICON_RU[f"мит{random_num}"])
+
+
+@router.message(MaskedTextFilter(r"\b(р+е+л+и+г+и+(?:я|и|ю|е|й|ям|ями|ях)?)\b"))
+async def handle_religion_text(message: Message, word: str):
+    await message.answer(f"Вы упомянули слово: {word}")
